3971b84d98a7ea27c3bda1504a5797a43f8473ca
Alvy Fauzi <alvyfauzi10@gmail.com>
2021-12-16 17:42:29 +0700

    add card

---
+++ src/apps/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from "../stores/Counter.js";
+
+export default configureStore({
+  reducer: {
+    // counter: counterReducer,
+  },
+});
