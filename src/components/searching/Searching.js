1e047e01f2472b7dd88512f55143068902d85cf3
Alvy Fauzi <alvyfauzi10@gmail.com>
2022-01-05 14:37:27 +0700

    fixed search component

--- src/components/searching/Searching.js
+++ src/components/searching/Searching.js
@@ -34,39 +34,64 @@ export default function Searching() {
               fluid
               style={{
                 backgroundColor: "rgba(255, 255, 255, 0.22)",
-                height: "130px",
+                height: "100%",
               }}
             >
-              <Row style={{ paddingTop: "45px" }}>
-                <Form inline className="d-flex" onSubmit={handleSearching}>
-                  <Form.Select
-                    size="sm"
-                    id="nationality"
-                    name="Filter"
-                    value={filter}
-                    onChange={(e) => setfilter(e.target.value)}
-                    style={{ width: "110px", marginRight: "20px" }}
+              <Row style={{ paddingTop: "50px" }}>
+                <Form inline onSubmit={handleSearching}>
+                  <Row
+                    // style={{ padding: "0px", margin: "0px" }}
+                    className="justify-content-center"
                   >
-                    <option value="complex">Complex</option>
-                    <option selected value="building">
-                      Building
-                    </option>
-                  </Form.Select>
-                  <Form.Control
-                    value={text}
-                    type="text"
-                    placeholder="Type Building (e.g. Wisma 46,Menara BCA)"
-                    variant="light"
-                    onChange={(e) => settext(e.target.value)}
-                    required
-                  />
-                  <Button
-                    variant="dark"
-                    type="submit"
-                    style={{ marginLeft: "20px" }}
-                  >
-                    Search
-                  </Button>
+                    <Col lg={2} style={{ padding: "0" }}>
+                      <Form.Select
+                        size="sm"
+                        id="nationality"
+                        name="Filter"
+                        value={filter}
+                        onChange={(e) => setfilter(e.target.value)}
+                        style={{
+                          width: "115px",
+                          marginRight: "20px",
+                          fontSize: "16px",
+                          margin: "auto",
+                        }}
+                      >
+                        <option value="complex">Complex</option>
+                        <option selected value="building">
+                          Building
+                        </option>
+                      </Form.Select>
+                    </Col>
+                    <Col lg={8}>
+                      <Form.Control
+                        value={text}
+                        type="text"
+                        placeholder="(e.g. Wisma 46,Menara Global)"
+                        variant="light"
+                        onChange={(e) => settext(e.target.value)}
+                        required
+                        style={{
+                          height: "35px",
+                          marginBottom: "5px",
+                        }}
+                      />
+                    </Col>
+                    <Col lg={2} style={{ padding: 0 }}>
+                      <Button
+                        variant="dark"
+                        type="submit"
+                        style={{
+                          width: "115px",
+
+                          height: "35px",
+                          fontSize: "15px",
+                        }}
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
                 </Form>
               </Row>
             </Container>
