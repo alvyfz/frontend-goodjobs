e23be2c6b7515033610135c86d09f58dd2f30ab1
iskandardevan <devvv13@gmail.com>
2022-01-06 16:25:22 +0700

    feature chat 30%

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,9 +1,20 @@
 import { Container, Row, Col, Spinner } from "react-bootstrap";
 import NavBar from "../../components/navbar/NavBar";
 import Footer from "../../components/footer/Footer";
-
+import "./Chat.css";
 import { Link } from "react-router-dom";
+import { parseCookies } from "nookies";
+import jwt_decode from "jwt-decode";
+import MessageInput from "../../components/message/MessageInput";
+import Message from "./Message";
+
 const Chat = () => {
+    const auth = parseCookies("auth").auth;
+    const jwtDefault =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwicm9sZV9pZCI6MCwiZXhwIjoxNjQwNTIzODE1fQ.RTtmDJ2fXyxY4N9GXWJnH-beaFIuHsgUSF3hJHHRXqU";
+    const jwt = jwt_decode(auth || jwtDefault);
+    const role_id = jwt.Role_ID;
+    console.log(jwt.Name)
     return(
         <>
             <NavBar chat={true} />
@@ -29,16 +40,20 @@ const Chat = () => {
             <Container fluid className="container2">
                 <Row className="justify-content-center">
                     <Col lg={4}>
-                        <Row className="justify-content-center">
-                            <Container >
-                                List Room Chat
+                        <Row>
+                            <Container>
+                                <div >
+                                    List Room Chat
+                                </div>
                             </Container>
                         </Row>
                     </Col>
                     <Col lg={6}>
-                        <Row className="justify-content-center">
-                            <Container  >
+                        <Row>
+                            <Container>
                                 Chat Content
+                                <Message></Message>
+                                <MessageInput></MessageInput>
                             </Container>
                         </Row>
                     </Col>
